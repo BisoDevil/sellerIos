467595eab11e87c0955ffe4e24a31e94b40a5a1a
Basem Abduallah <bisodevil@macbook-pro.eggate.local>
2019-04-08 09:30:36 +0200

    Initial Commit

---
+++ EG-Gate Seller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EG-Gate Seller
+//
+//  Created by Basem Abduallah on 4/8/19.
+//  Copyright © 2019 Basem Abduallah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
